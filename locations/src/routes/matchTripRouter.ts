9983b97678ebaff6a1622dc2cce7aeebba0da675
Aly Hussein <111089237+alyHusseinn@users.noreply.github.com>
2024-11-13 21:45:37 +0200

    fix(match): fix the expected body

--- locations/src/routes/matchTripRouter.ts
+++ locations/src/routes/matchTripRouter.ts
@@ -6,8 +6,8 @@ import TripsService from "../services/tripsService";
 const router = Router();
 
 const zodSchema = z.object({
-    id: z.number(),
-    riderId: z.number(),
+    ID: z.number(),
+    rider_id: z.number(),
     origin: z.object({
         lat: z.number(),
         long: z.number(),
@@ -25,12 +25,14 @@ const zodSchema = z.object({
 function matchRouter(io: Server): Router {
     router.post("/", async (req, res) => {
         try {
-            const { id, riderId, origin, destination } = zodSchema.parse(req.body);
+            const { ID, rider_id, origin, destination } = zodSchema.parse(req.body);
 
-            const trip = await TripsService.createOne(id, riderId, origin, destination);
+            const trip = await TripsService.createOne(ID, rider_id, origin, destination);
+            console.log("Trip created:", trip);
             TripsService.matchTrip(io, trip);
             res.status(200).json({ message: "New trip added, looking for a driver...." });
         } catch (error) {
+            console.error(error);
             if (error instanceof z.ZodError) {
                 res.status(400).json({ error: "Invalid request body" });
             } else {
